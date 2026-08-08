36d865d03e935561839138ad84b16bfeb8966e7d
robbray-dev <rbray77777@gmail.com>
2026-08-08 02:55:34 +0000

    base for pages init

---
+++ job-intel-ui/src/app/base-for-pages/base-for-pages.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-for-pages',
+  imports: [],
+  templateUrl: './base-for-pages.html',
+  styleUrl: './base-for-pages.css',
+})
+export class BaseForPages {}
