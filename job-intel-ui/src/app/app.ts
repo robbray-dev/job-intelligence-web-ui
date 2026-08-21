bd11c73d803e6dacafbda0156188576dd7982ef4
robbray-dev <rbray77777@gmail.com>
2026-08-21 14:54:10 +0000

    added header to browse jobs page

--- job-intel-ui/src/app/app.ts
+++ job-intel-ui/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Banner } from './banner/banner';
+import { BrowseJobsPage } from './browse-jobs-page/browse-jobs-page';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Banner],
+  imports: [RouterOutlet, Banner, BrowseJobsPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
