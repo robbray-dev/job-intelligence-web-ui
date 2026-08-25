a5ffc8ff6057d185db6f578338686c2b83a1c40c
robbray-dev <rbray77777@gmail.com>
2026-08-25 02:41:07 +0000

    toggle button in the filter box

--- job-intel-ui/src/app/browse-jobs-page/filter-box/filter-box.ts
+++ job-intel-ui/src/app/browse-jobs-page/filter-box/filter-box.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { NgToggleComponent } from 'ng-toggle-button';
 
 @Component({
   selector: 'app-filter-box',
-  imports: [MatCheckboxModule],
+  imports: [MatCheckboxModule, NgToggleComponent],
   templateUrl: './filter-box.html',
   styleUrl: './filter-box.css',
 })
