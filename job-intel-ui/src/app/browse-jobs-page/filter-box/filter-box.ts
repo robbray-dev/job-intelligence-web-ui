a120347c52eef690107581d2dadae67f4605e838
robbray-dev <rbray77777@gmail.com>
2026-08-26 02:54:17 +0000

    ngx slider range slider

--- job-intel-ui/src/app/browse-jobs-page/filter-box/filter-box.ts
+++ job-intel-ui/src/app/browse-jobs-page/filter-box/filter-box.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { NgToggleComponent } from 'ng-toggle-button';
 import { MatButtonModule } from '@angular/material/button';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-filter-box',
-  imports: [MatCheckboxModule, NgToggleComponent, MatButtonModule],
+  imports: [MatCheckboxModule, NgToggleComponent, MatButtonModule, NgxSliderModule],
   templateUrl: './filter-box.html',
   styleUrl: './filter-box.css',
 })
-export class FilterBox {}
+export class FilterBox {
+  value: number = 40;
+  highValue: number = 60;
+  options: Options = {
+    floor: 0,
+    ceil: 100,
+  };
+}
