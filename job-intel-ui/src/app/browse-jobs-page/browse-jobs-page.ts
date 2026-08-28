ca3c0ef195882afe25098a5345f5e29b18f613c9
robbray-dev <rbray77777@gmail.com>
2026-08-28 02:43:30 +0000

    imports for search bar

--- job-intel-ui/src/app/browse-jobs-page/browse-jobs-page.ts
+++ job-intel-ui/src/app/browse-jobs-page/browse-jobs-page.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { FilterBox } from './filter-box/filter-box';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-browse-jobs-page',
-  imports: [FilterBox],
+  imports: [FilterBox, MatFormFieldModule, MatIconModule, MatInputModule],
   templateUrl: './browse-jobs-page.html',
   styleUrl: './browse-jobs-page.css',
 })
