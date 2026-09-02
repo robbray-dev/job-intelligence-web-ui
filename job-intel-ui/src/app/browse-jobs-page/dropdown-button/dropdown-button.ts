1180f2c038f623d9a60398a5bbd04190a1c0c6c6
robbray-dev <rbray77777@gmail.com>
2026-09-02 03:45:32 +0000

    init of the dropdown button

--- job-intel-ui/src/app/browse-jobs-page/dropdown-button/dropdown-button.ts
+++ job-intel-ui/src/app/browse-jobs-page/dropdown-button/dropdown-button.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import {afterRenderEffect, computed, signal, viewChild} from '@angular/core';
+import {Combobox, ComboboxPopup, ComboboxWidget} from '@angular/aria/combobox';
+import {Listbox, Option} from '@angular/aria/listbox';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-dropdown-button',
-  imports: [],
+  imports: [Combobox, ComboboxPopup, ComboboxWidget, Listbox, Option, OverlayModule],
   templateUrl: './dropdown-button.html',
   styleUrl: './dropdown-button.css',
 })
-export class DropdownButton {}
+export class DropdownButton {
+    readonly listbox = viewChild(Listbox);
+
+  readonly selectedValues = signal<string[]>([]);
+  readonly displayValue = computed(() => this.selectedValues()[0] || 'Select a label');
+  readonly popupExpanded = signal(false);
+
+  readonly labels = [
+    'Important',
+    'Starred',
+    'Work',
+    'Personal',
+    'To Do',
+    'Later',
+    'Read',
+    'Travel',
+  ];
+
+  constructor() {
+    afterRenderEffect(() => {
+      this.listbox()?.scrollActiveItemIntoView();
+    });
+  }
+
+  onCommit() {
+    this.popupExpanded.set(false);
+  }
+}
