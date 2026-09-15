d6c24e1e8184f848584bcc3a130a57a6fa2be906
robbray-dev <rbray77777@gmail.com>
2026-09-15 22:16:36 +0000

    interface for job

--- job-intel-ui/src/app/Service/dummy-api.ts
+++ job-intel-ui/src/app/Service/dummy-api.ts
@@ -1,4 +1,16 @@
 import { Service } from '@angular/core';
 
+export interface Job {
+  id: number;
+  title: string;
+  location: string;
+  companyName: string;
+  salaryMin: number;
+  salaryMax: number;
+  description: string;
+  jobUrl: string;
+  postedDate: string;
+  skills: string[];
+}
 @Service()
 export class DummyApi {}
