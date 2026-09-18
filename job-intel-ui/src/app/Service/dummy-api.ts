46e97806753461ecc537457e0d5107b2ab578c12
robbray-dev <rbray77777@gmail.com>
2026-09-18 02:44:14 +0000

    added the job response type

--- job-intel-ui/src/app/Service/dummy-api.ts
+++ job-intel-ui/src/app/Service/dummy-api.ts
@@ -2,6 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Service, Signal } from '@angular/core';
 import { Observable } from 'rxjs';
 
+export interface JobResponse {
+  jobs: Job[];
+}
 export interface Job {
   id: number;
   title: string;
@@ -19,11 +22,21 @@ export interface Job {
 export class DummyApi {
   private http = inject(HttpClient);
 
-  fetchJsonData(): Observable<Job[]> {
+  fetchJsonData(): Observable<JobResponse> {
     // Path to the JSON file (relative to the data folder)
     const jsonUrl = 'data/jobs.json';
 
     // Use http.get() to fetch the file. Returns an Observable.
-    return this.http.get<Job[]>(jsonUrl);
+    return this.http.get<JobResponse>(jsonUrl);
+  }
+
+  fetchLocalJsonData(): Job[] {
+    const jsonStr =
+      '[{"id": 1, "title": "Senior Backend Engineer","skills": ["AWS","Java","Spring Boot", "PostgreSQL"]}]';
+
+    const jsonArray: Job[] = JSON.parse(jsonStr);
+
+    console.log(jsonArray);
+    return jsonArray;
   }
 }
