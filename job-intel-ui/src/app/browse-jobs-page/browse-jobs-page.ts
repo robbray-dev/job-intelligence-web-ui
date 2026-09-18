d98f5e41198f1957db8ede3596404ecdf6538f21
robbray-dev <rbray77777@gmail.com>
2026-09-18 02:43:42 +0000

    figured out how to run through the jobs

--- job-intel-ui/src/app/browse-jobs-page/browse-jobs-page.ts
+++ job-intel-ui/src/app/browse-jobs-page/browse-jobs-page.ts
@@ -4,7 +4,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { DropdownButton } from './dropdown-button/dropdown-button';
-import { DummyApi, Job } from '../Service/dummy-api';
+import { DummyApi, Job, JobResponse } from '../Service/dummy-api';
 
 @Component({
   selector: 'app-browse-jobs-page',
@@ -13,7 +13,7 @@ import { DummyApi, Job } from '../Service/dummy-api';
   styleUrl: './browse-jobs-page.css',
 })
 export class BrowseJobsPage implements OnInit {
-  jobs: Job[] = [];
+  jobs: JobResponse | undefined;
 
   constructor(private service: DummyApi) {}
 
@@ -21,7 +21,22 @@ export class BrowseJobsPage implements OnInit {
     this.service.fetchJsonData().subscribe({
       next: (data) => {
         this.jobs = data;
-        console.log('JSON Data Loaded:', this.jobs);
+        const temps = this.jobs;
+        console.log('JSON Data Loaded:', this.jobs.jobs);
+        console.log(typeof this.jobs);
+        console.log('temps', temps);
+        var keys = Object.keys(this.jobs);
+        console.log('keys', keys);
+
+        console.log('something', this.jobs);
+        console.log('JSON Data:', this.jobs.jobs);
+        const jobArray = this.jobs.jobs;
+
+        jobArray.forEach((job: Job) => {
+          console.log('job title here', job.title);
+        });
+        let arr: string[] = ['here', 'hs'];
+        console.log('yer:', arr);
       },
       error: (err) => {
         console.error('Error loading JSON data:', err);
