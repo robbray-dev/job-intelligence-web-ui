f54d212c6085d3d887fba967b00696fa3e05d26d
robbray-dev <rbray77777@gmail.com>
2026-09-19 00:51:40 +0000

    removed console log statements

--- job-intel-ui/src/app/browse-jobs-page/browse-jobs-page.ts
+++ job-intel-ui/src/app/browse-jobs-page/browse-jobs-page.ts
@@ -21,22 +21,11 @@ export class BrowseJobsPage implements OnInit {
     this.service.fetchJsonData().subscribe({
       next: (data) => {
         this.jobs = data;
-        const temps = this.jobs;
-        console.log('JSON Data Loaded:', this.jobs.jobs);
-        console.log(typeof this.jobs);
-        console.log('temps', temps);
-        var keys = Object.keys(this.jobs);
-        console.log('keys', keys);
-
-        console.log('something', this.jobs);
-        console.log('JSON Data:', this.jobs.jobs);
         const jobArray = this.jobs.jobs;
 
         jobArray.forEach((job: Job) => {
           console.log('job title here', job.title);
         });
-        let arr: string[] = ['here', 'hs'];
-        console.log('yer:', arr);
       },
       error: (err) => {
         console.error('Error loading JSON data:', err);
