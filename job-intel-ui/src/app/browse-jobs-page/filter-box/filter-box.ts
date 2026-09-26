68418a4a89a4da78882e6e696182ced3504d5f4d
robbray-dev <rbray77777@gmail.com>
2026-09-26 00:12:04 +0000

    remvoed the ngx slider component setup

--- job-intel-ui/src/app/browse-jobs-page/filter-box/filter-box.ts
+++ job-intel-ui/src/app/browse-jobs-page/filter-box/filter-box.ts
@@ -2,21 +2,13 @@ import { Component } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { NgToggleComponent } from 'ng-toggle-button';
 import { MatButtonModule } from '@angular/material/button';
-import { NgxSliderModule } from '@angular-slider/ngx-slider';
-import { Options } from '@angular-slider/ngx-slider';
-import {MatDividerModule} from '@angular/material/divider';
+
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-filter-box',
-  imports: [MatCheckboxModule, NgToggleComponent, MatButtonModule, NgxSliderModule, MatDividerModule],
+  imports: [MatCheckboxModule, NgToggleComponent, MatButtonModule, MatDividerModule],
   templateUrl: './filter-box.html',
   styleUrl: './filter-box.css',
 })
-export class FilterBox {
-  value: number = 40;
-  highValue: number = 60;
-  options: Options = {
-    floor: 0,
-    ceil: 100,
-  };
-}
+export class FilterBox {}
