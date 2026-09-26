9009e38328244d7be806aa109d20c2ba99e09635
robbray-dev <rbray77777@gmail.com>
2026-09-26 02:23:55 +0000

    attempting to mock the fetch local json call

--- job-intel-ui/src/app/browse-jobs-page/browse-jobs-page.spec.ts
+++ job-intel-ui/src/app/browse-jobs-page/browse-jobs-page.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BrowseJobsPage } from './browse-jobs-page';
+import { vi } from 'vitest';
+import { DummyApi } from '../Service/dummy-api';
 
 describe('BrowseJobsPage', () => {
   let component: BrowseJobsPage;
@@ -9,6 +11,7 @@ describe('BrowseJobsPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BrowseJobsPage],
+      providers: [{ provide: DummyApi, useValue: vi.mockObject(DummyApi) }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BrowseJobsPage);
